848a02b68eaea366ece410c2717872de5d6861ad
GiacomoGiovannetti <giovannettii.giacomo@gmail.com>
2023-05-26 20:37:10 +0200

    aggiunta event delegation

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll('.btn');
+const buttonsContainer = document.querySelector('#btn-container');
 const valore = document.querySelector('#value');
 const expAmount = document.querySelector('#exp');
 const lvlValue = document.querySelector('#lvl-value');
@@ -17,45 +18,44 @@ lvlValue.textContent = lvlCount;
     click dei pulsanti e per sound effect al click dei pulsanti */
 //dichiarazione funzione di incremento/decremento/reset della barra dell'esperienza al click dei pulsanti 
 
-buttons.forEach(function(btn) {
-    btn.addEventListener('click', function(e){
-        const styles = e.currentTarget.id;
-        if(styles.includes('btn-10')){
-            if(count <= 9){
-                count = 0; 
-            }else{
-                count-= 10;
-            }
-            expWidth-= 100;
-        } 
+buttonsContainer.addEventListener('click', function(e){
+    const styles = e.target.id;
+    console.log(styles);
+         if(styles.includes('btn-10')){
+             if(count <= 9){
+                 count = 0; 
+             }else{
+                 count-= 10;
+             }
+             expWidth-= 100;
+         } 
         else if(styles.includes('decrement') && count > 0 ){
-            count--;
-            expWidth-= 10;
-        }
-        else if(styles.includes('incr-10')){
-            count+= 10;
-            expWidth += 100;
-        }
-        else if(styles.includes('increment')){
-            count++;
-            expWidth+= 10;
-        }
-        else if(styles.includes('reset')){
-            count = 0;
-            lvlCount = 1;
-            expWidth -= expWidth;
-        }
-        expAmount.style.width = `${expWidth}%`;
-        valore.textContent = count ;
+             count--;
+             expWidth-= 10;
+         }
+         else if(styles.includes('incr-10')){
+             count+= 10;
+             expWidth += 100;
+         }
+         else if(styles.includes('increment')){
+             count++;
+             expWidth+= 10;
+         }
+         else if(styles.includes('reset')){
+             count = 0;
+             lvlCount = 1;
+             expWidth -= expWidth;
+         }
+         expAmount.style.width = `${expWidth}%`;
+         valore.textContent = count ;
         
-        //funzioni per la barra dell'esperienza quando si verifica un level up/level down e per nascondere il pop-up
-        hideLvlPopUp();
-        lvlUpByPlus10(styles);
-        lvlUpByPlus1(styles);
-        lvlDownByMinus10(styles , count);
-        lvlDownByMinus1(styles , count);
+         //funzioni per la barra dell'esperienza quando si verifica un level up/level down e per nascondere il pop-up
+         hideLvlPopUp();
+         lvlUpByPlus10(styles);
+         lvlUpByPlus1(styles);
+         lvlDownByMinus10(styles , count);
+         lvlDownByMinus1(styles , count);
         
-        lvlValue.textContent = lvlCount;
-        clickSound.play();
-    })
-});
+         lvlValue.textContent = lvlCount;
+         clickSound.play();
+     })
